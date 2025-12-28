3ac26ebc96cb3ba59de86f5a035350343eb22fa2
lafv <armanyfelix@proton.me>
2025-12-27 22:45:43 -0800

    symlinks fixed

--- nixos/home.nix
+++ nixos/home.nix
@@ -3,15 +3,15 @@
 {
   home.username = "lafv";
   home.homeDirectory = "/home/lafv";
-  home.stateVersion = "25.11";
+  home.stateVersion = "25.05";
 
-  home.file.".zshrc".source = /home/lafv/Dotfiles/.zshrc;
-  home.file.".p10k.zsh".source = /home/lafv/Dotfiles/.p10k.zsh;
-  home.file.".config/nvim".source = /home/lafv/Dotfiles/.config/nvim;
-  home.file.".config/yazi".source = /home/lafv/Dotfiles/.config/yazi;
-  home.file.".config/niri".source = /home/lafv/Dotfiles/.config/niri;
-  home.file.".config/wezterm".source = /home/lafv/Dotfiles/.config/wezterm;
-  home.file.".config/zed".source = /home/lafv/Dotfiles/.config/zed;
+  home.file.".zshrc".source = /home/lafv/dotfiles/.zshrc;
+  home.file.".p10k.zsh".source = /home/lafv/dotfiles/.p10k.zsh;
+  home.file.".config/nvim".source = /home/lafv/dotfiles/.config/nvim;
+  home.file.".config/yazi".source = /home/lafv/dotfiles/.config/yazi;
+  home.file.".config/niri".source = /home/lafv/dotfiles/.config/niri;
+  home.file.".config/wezterm".source = /home/lafv/dotfiles/.config/wezterm;
+  home.file.".config/zed".source = /home/lafv/dotfiles/.config/zed;
 
   home.packages = with pkgs; [
     tree
